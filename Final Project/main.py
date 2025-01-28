a0a87c15ef2505f3db6913bc0583e9a57c311c59
fahmida0404 <153936021+fahmida0404@users.noreply.github.com>
2025-01-28 22:26:56 +0600

    API

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,29 +1,54 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import uvicorn
-
 from app.routers import news, summary
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI(
-
     title="News Summarizing API",
-    description="This is the API documentation for news scraping and summarizing using AI (Llama).",
+    version="0.2",
+    description="This is the API created for News Scraping and Summarizing through Llama.",
     contact={
         "name": "Fahmida Akter",
         "email": "fahmidaakter0404@gmail.com",
     },
-    redoc_url="/details",
+    redoc_url="/documentation",
     docs_url="/execute",
 )
+
+# Define CORS settings
+origins = ["*"]  # Allow requests from any origin
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(news.router)
 app.include_router(summary.router)
 
-
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the News Summarizing API"}
+    return {"message": "Welcome!"}
+
+
+@app.get("/html", response_class=HTMLResponse)
+def html_message():
+    message = '''
+    <div style=" text-align: center; padding: 100px;  color: teal; font-size: 36px;">
+        Welcome to the News Summarizing API!<br>
+        Created by: Fahmida Akter (C201221).
+    </div>
+    '''
+    return message
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8001, reload=True)
-    # uvicorn.run("main:app", host="localhost", port=8011, reload=True)
+
+    uvicorn.run("main:app", host="localhost", port=3000, reload=True)
